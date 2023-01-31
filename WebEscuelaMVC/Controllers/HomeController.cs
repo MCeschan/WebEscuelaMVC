f04491403e3afb88a4d587aaddb4a2d5c0e9cbe1
Anais Moreno <anabet.94@gmail.com>
2023-01-31 14:57:58 -0300

    HomeController

---
+++ WebEscuelaMVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebEscuelaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Encabezado = "Bienvenido al Portal de la Escuela nombre";
+            ViewBag.Fecha = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
